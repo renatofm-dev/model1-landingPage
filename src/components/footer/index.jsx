6491d834f5ef2f139a9c0b748a3a5ac0fb88aac1
Renato Martins <rmartins6694@gmail.com>
2023-03-10 14:29:17 -0300

    change colors and links

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Styles from "./footer.module.scss";
-import Logo from "/public/images/logo.svg";
+import Logo from "/public/images/logo60x60.svg";
 import IconInstagram from "/public/images/icon-instagram.svg";
 import IconFacebook from "/public/images/icon-facebook.svg";
 import IconLinkedin from "/public/images/icon-linkedin.svg";
@@ -12,8 +12,8 @@ const Footer = () => {
     <div className={Styles.container}>
       <div className={`${Styles.column} ${Styles.columnPrincipal}`}>
         <Image src={Logo} alt="Logo" />
-        <h1 className={Styles.title}>0800 800 800</h1>
-        <p>comercial@agencia.com.br</p>
+        <h1 className={Styles.title}>+55 (61) 98557-6100</h1>
+        <p>contato@holdingsystem.com.br</p>
       </div>
       <div className={Styles.column}>
         <h1>MENU</h1>
@@ -23,27 +23,27 @@ const Footer = () => {
       <div className={Styles.column}>
         <h1>CONTEÚDO</h1>
         <p>E-books</p>
-        <p>Fórmulas prontas</p>
+        <p>Blog</p>
       </div>
       <div className={`${Styles.column} ${Styles.alignRight}`}>
         <h1>SOCIAL</h1>
         <div className={Styles.icons}>
-          <Link href="/">
+          <Link href="https://www.instagram.com/holdingsystem/">
             <Image src={IconInstagram} alt="Icon" className={Styles.icon} />
           </Link>
-          <Link href="/">
+          <Link href="https://www.facebook.com/holdingsystem">
             <Image src={IconFacebook} alt="Icon" className={Styles.icon} />
           </Link>
           <Link href="/">
             <Image src={IconLinkedin} alt="Icon" className={Styles.icon} />
           </Link>
-          <Link href="/">
+          <Link href="https://www.youtube.com/@holdingsystembrazil">
             <Image src={IconYoutube} alt="Icon" className={Styles.icon} />
           </Link>
         </div>
       </div>
       <div className={Styles.allRightReserved}>
-        ©2022 AGENCIA - Todos os direitos reservados.
+        ©2022 Holding System Brazil - Todos os direitos reservados.
       </div>
     </div>
   );
