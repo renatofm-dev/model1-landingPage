d056ab446ea68fb4e5f973157210287edeb6ea61
Renato Martins <rmartins6694@gmail.com>
2023-03-12 11:26:38 -0300

    add new icons

--- src/components/features/index.jsx
+++ src/components/features/index.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
 import Styles from "./features.module.scss";
-import Icon from "/public/images/icon.svg";
+import Icon from "/public/images/Icone-01.svg";
+import Icon1 from "/public/images/Icone-02.svg";
+import Icon2 from "/public/images/Icone-03.svg";
 
 const Features = () => {
   return (
     <div className={Styles.container}>
-      <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
+      <h1>Soluções Personalizadas e Eficazes para Preservar seu Patrimônio.</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
+      Equipe capacitada em Brasília, Garantimos o seu Sucesso Financeiro.
+      Faça seu Orçamento com um Especialista.
       </p>
 
       <div className={Styles.features}>
         <div className={Styles.feature}>
-          <Image src={Icon} alt="Icon" className={Styles.icon} />
+          <Image src={Icon2} alt="Icon" className={Styles.icon} />
           <h1>Planejamento Patrimonial</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -21,7 +23,7 @@ const Features = () => {
           </p>
         </div>
         <div className={Styles.feature}>
-          <Image src={Icon} alt="Icon" className={Styles.icon} />
+          <Image src={Icon2} alt="Icon" className={Styles.icon} />
           <h1>Planejamento Sucessório</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -29,7 +31,7 @@ const Features = () => {
           </p>
         </div>
         <div className={Styles.feature}>
-          <Image src={Icon} alt="Icon" className={Styles.icon} />
+          <Image src={Icon1} alt="Icon" className={Styles.icon} />
           <h1>Proteção Patrimonial</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
