509c5f0f592ca13d65d66ad14bb356b8a3dc446a
Renato F. Martins <rmartins6694@gmail.com>
2023-05-16 09:09:03 -0300

    add some things

--- src/components/wppButton/index.jsx
+++ src/components/wppButton/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-export default function WppButton() {
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
+const WppButton = () => {
   return (
-      <FloatingWhatsApp 
-        phoneNumber='+5561985576100'
-        accountName='Gerente de Relacionamento'
-        statusMessage='Tempo de resposta 20 minutos'
-        chatMessage='Olá, sou a Bia! Gerente de relacionamento da Holding System Brazil. Como posso ajudar?'
-        placeholder='Escreva sua mensagem...'
-        messageDelay={1}
-        allowClickAway={true}
-        notification={true}
-        chatboxHeight={400}
-      />
+    <FloatingWhatsApp
+      phoneNumber='+5561985576100'
+      accountName='Gerente de Relacionamento'
+      statusMessage='Tempo de resposta 20 minutos'
+      chatMessage='Olá, sou a Bia! Gerente de relacionamento da Holding System Brazil. Como posso ajudar?'
+      placeholder='Escreva sua mensagem...'
+      messageDelay={1}
+      allowClickAway={true}
+      notification={true}
+      chatboxHeight={400}
+      darkMode={false}
+      // onClick={() => {location.href='/'}}
+
+    />
   )
 }
+
+export default WppButton
